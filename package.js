556c42508c54e39a7c2d6cbe7174b7ffc025ad8e
akirsila <54467559+akirsila@users.noreply.github.com>
2025-09-19 18:17:40 +0300

    Update package.js

--- package.js
+++ package.js
@@ -16,3 +16,32 @@
   "express-session": "^1.17.3"
   }
 }
+{
+  "name": "tavarakyyti-chat",
+  "version": "1.0.0",
+  "description": "Tavarakyyti chat module (REST + Socket.IO + S3 + Admin)",
+  "main": "server/index.js",
+  "scripts": {
+    "start": "node server/index.js",
+    "dev": "nodemon server/index.js"
+  },
+  "dependencies": {
+    "@aws-sdk/client-s3": "^3.637.0",
+    "@aws-sdk/s3-request-presigner": "^3.637.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1",
+    "express": "^4.19.2",
+    "express-rate-limit": "^7.4.0",
+    "helmet": "^7.0.0",
+    "jsonwebtoken": "^9.0.2",
+    "mongoose": "^8.3.2",
+    "multer": "^1.4.5-lts.1",
+    "socket.io": "^4.7.5"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.0"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
